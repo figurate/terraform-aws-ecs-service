95201058c1453acb9be94a952b1ec767d6a09c9e
Ben Fortuna <bfortuna@odecee.com.au>
2020-09-11 20:36:42 +1000

    Added diagram

--- diagram.py
+++ diagram.py
@@ -1,12 +1,24 @@
-from diagrams import Diagram, Cluster
-from diagrams.aws.compute import ECS, AutoScaling
-from diagrams.aws.network import APIGateway, CloudMap
+from diagrams import Diagram, Cluster, Edge
+from diagrams.aws.compute import ECS
+from diagrams.aws.network import APIGateway, CloudMap, ELB
+from diagrams.generic.blank import Blank
 
 with Diagram("AWS ECS Service", show=False, direction="TB"):
 
     api_gw = APIGateway("api gateway")
 
+    cloud_map = CloudMap("cloud map")
+    api_gw << cloud_map
+
     with Cluster("vpc"):
 
-        api_gw >> CloudMap("service discovery") >> ECS("ecs cluster") >> AutoScaling("capacity provider")
+        target_group = ELB("target_group (+)")
 
+    with Cluster("ecs"):
+
+     service = ECS("ecs service")
+     api_gw >> service >> cloud_map
+
+     service >> Edge(label="container:port (+)") >> target_group
+
+    Blank("+ 1..n")
